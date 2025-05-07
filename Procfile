ad768ef9c1b97151dd4b84fd18060cc7019a61c3
Bret Meraki <bretmeraki@gmail.com>
2025-05-07 11:59:56 -0400

    Clean up repo: remove unnecessary deployment files

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: python debug_suite/flask_api/flask_dashboard.py
--- wsgi.py
+++
@@ -1,8 +0,0 @@
-"""
-WSGI entry point for compatibility with the default gunicorn startup command.
-"""
-import os
-from debug_suite.flask_api.flask_dashboard import app
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
